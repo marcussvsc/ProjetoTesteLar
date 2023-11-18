36bdc459fc453125669c6b7a7ea13ea09a8bc7cd
Marcus Carvalho <marcuss.vsc@gmail.com>
2023-11-18 15:39:21 -0300

    Iniciado implementação do relacionamento de Pessoa com Endereço

--- Persistence/PessoasDbContext.cs
+++ Persistence/PessoasDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ProjetoTesteLar.DTOs;
+using ProjetoTesteLar.Entities;
 
 namespace ProjetoTesteLar.Persistence
 {
@@ -13,12 +14,9 @@ namespace ProjetoTesteLar.Persistence
             builder.HasMany(e => e.Telefones);  
             builder.HasMany(e => e.Enderecos)
                    .WithMany(e => e.Pessoas);
-            //modelBuilder.Entity<Pessoa>()
-            //   .HasMany(e => e.Enderecos)
-            //   .WithMany(e => e.Pessoas)
-            //   .UsingEntity<PessoaEndereco>(
-            //       l => l.HasOne<Endereco>().WithMany().HasForeignKey(e => e.EnderecoId),
-            //       r => r.HasOne<Pessoa>().WithMany().HasForeignKey(e => e.PessoaId));
+            builder.HasMany(e => e.Enderecos)
+               .WithMany(e => e.Pessoas)
+               .UsingEntity<PessoaEndereco>();
 
         }
     }
